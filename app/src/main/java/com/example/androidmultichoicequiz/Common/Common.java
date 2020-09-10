1a2525ad36e9ac7e55495a8306303c4d9e7b9653
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2020-09-10 01:56:33 -0400

    retrive back

--- app/src/main/java/com/example/androidmultichoicequiz/Common/Common.java
+++ app/src/main/java/com/example/androidmultichoicequiz/Common/Common.java
@@ -1,9 +1,22 @@
 package com.example.androidmultichoicequiz.Common;
 
+import android.os.CountDownTimer;
+
 import com.example.androidmultichoicequiz.Model.Category;
+import com.example.androidmultichoicequiz.Model.CurrentQuestion;
+import com.example.androidmultichoicequiz.Model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Common {
+    public static final int TOTAL_TIME =20*60*1000 ;
+    public static List<Question> questionList=new ArrayList<>();
     public static Category selectedCategory=new Category();
+    public  static List<CurrentQuestion > answerSheetList=new ArrayList<>();
+
+
+    public static CountDownTimer countDownTimer;
 
     public enum ANSWER_TYPE{
         NO_ANSWER,
